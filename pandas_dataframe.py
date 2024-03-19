d5d62e9f93552a85a1076d97858f14b05fc1d0d8
jungbal <terykid@naver.com>
2024-03-19 11:18:51 +0900

    dictionary를 이용해 DataFrame 생성

--- pandas_dataframe.py
+++ pandas_dataframe.py
@@ -5,6 +5,8 @@ data = {'ID' : ['A1', 'B1', 'C1', 'D1', 'E1'],
         'X1' : [1, 2, 3, 4, 5],
         'X2' : [3.0, 4.5, 3.2, 4.0, 3.5]}
 
+print(data)
+
 # dataframe 형식으로 변환(index로 라벨 추가)
 mydata = pd.DataFrame(data, index=['a', 'b', 'c', 'd', 'e'])
 
