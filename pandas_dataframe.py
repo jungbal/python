fd76f82bbbe53bf7ead96964108ff980fce82215
jungbal <terykid@naver.com>
2024-03-19 11:12:04 +0900

    dictionary dataframe으로 변환

---
+++ pandas_dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# dictionary 생성
+data = {'ID' : ['A1', 'B1', 'C1', 'D1', 'E1'],
+        'X1' : [1, 2, 3, 4, 5],
+        'X2' : [3.0, 4.5, 3.2, 4.0, 3.5]}
+
+# dataframe 형식으로 변환(index로 라벨 추가)
+mydata = pd.DataFrame(data, index=['a', 'b', 'c', 'd', 'e'])
+
+print(mydata)
